214be615dab604cb7b5fe01965584bde4a1a2a49
emmanuelvillada <124715910+emmanuelvillada@users.noreply.github.com>
2023-02-22 17:01:40 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite el año ");
+            int year = Int32.Parse(Console.ReadLine()) ;
+
+            Console.WriteLine("digite el año ");
+            int month = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite el año ");
+            int day = Int32.Parse(Console.ReadLine());
+            try
+            {
+                var object_date = new Date(year, month, day);
+                Console.WriteLine(object_date.ToString());
+            } catch(Exception ex) 
+            { 
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
