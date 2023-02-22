20ad6da312e9650d9529100f0f5619ca9543a851
emmanuelvillada <124715910+emmanuelvillada@users.noreply.github.com>
2023-02-22 17:00:40 -0500

    Add files via upload

---
+++ BaseCommissionEmployee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class BaseCommissionEmployee : Comission 
+    {
+        #region Properties
+        public decimal Base { get; set; }
+        #endregion
+
+        #region Methods
+        public override decimal GetValueToPay()
+        {
+            return base.GetValueToPay() + Base;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"\n\tSalary Base................{Base:C2}" +
+                $"\n\tValue to pay...............{GetValueToPay():C2}";
+        }
+        #endregion
+    }
+}
+
---
+++ salary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class salary : Employee
+    {
+        #region Properties
+
+        public decimal Salary { get; set; }
+
+        #endregion
+
+        #region Methods
+        public override decimal GetValueToPay()
+        {
+            return Salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}\n\t" +
+                $"Value to Pay........{GetValueToPay():C2}";
+        }
+
+        #endregion
+    }
+}
