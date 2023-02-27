90bb507c74de298cfa5e2e4b125e321102fb6e2d
EMMANUEL VILLADA SUAREZ <emmanuelvillada310016@correo.itm.edu.co>
2023-02-26 19:24:21 -0500

    program modificated

--- Program.cs
+++ Program.cs
@@ -1,27 +1,35 @@
 ﻿using System;
 
+
 namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("digite el año ");
-            int year = Int32.Parse(Console.ReadLine()) ;
+            Console.Writeline("how many employes do you want to put?");
+            int number_employes = Convert.ToInt(Console.Readline());
 
-            Console.WriteLine("digite el año ");
-            int month = Int32.Parse(Console.ReadLine());
-
-            Console.WriteLine("digite el año ");
-            int day = Int32.Parse(Console.ReadLine());
-            try
+            for (int i = 0, i < number_employes, i++)
             {
-                var object_date = new Date(year, month, day);
-                Console.WriteLine(object_date.ToString());
-            } catch(Exception ex) 
-            { 
-                Console.WriteLine(ex.Message);
+                Console.WriteLine("digite el año ");
+                int year = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("digite el año ");
+                int month = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("digite el año ");
+                int day = Int32.Parse(Console.ReadLine());
+                try
+                {
+                    var object_date = new Date(year, month, day);
+                    Console.WriteLine(object_date.ToString());
+                } catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
+
         }
     }
 }
